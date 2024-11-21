cc4cba15d4302be72d94432ddf4658a51976a397
Cyprian Kazimierz <cyprian.bartosik@cirvianum.cat>
2024-11-21 19:37:08 +0100

    1.3 (Fitxer Program.cs tornem a modificar els cotxes)

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ class Program
     static void Main(string[] args)
     {
         // Crear un objecte Vehicle
-        Vehicle vehicle1 = new Vehicle("Astra", "Opel", 2015, 180.0);
+        Vehicle vehicle1 = new Vehicle("Cordoba", "Seat", 2012, 210.0);
 
         // Mostrar la informació del vehicle
         vehicle1.MostrarInformacio();
 
         // Crear un altre objecte Vehicle
-        Vehicle vehicle2 = new Vehicle("Vectra", "Opel", 2008, 200.0);
+        Vehicle vehicle2 = new Vehicle("Ibiza", "Seat", 2017, 170.0);
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
